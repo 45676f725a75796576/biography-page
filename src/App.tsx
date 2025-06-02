705be3df8057bac7553137ba01f735211b44e706
45676f725a75796576 <156612784+45676f725a75796576@users.noreply.github.com>
2025-06-02 14:17:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
 import Navigation from './Navigation.tsx'
+// @ts-ignore
 import { Trans, useTranslation } from 'react-i18next'
 
 
-
+// @ts-ignore
 const languages = [
     { value: 'en', name: 'English' },
     { value: 'cs', name: 'Czech' },
@@ -17,6 +18,7 @@ const languages = [
 ]
 
 function App() {
+    // @ts-ignore
   const { t, i18n } = useTranslation();
 
   return (
